3eb1ec217810ce96e44cff20276fe033436a42db
Vlas Bochkarev <vlas.bochkarev@gmail.com>
2022-08-11 18:38:01 +0300

    no message

--- Assets/Scripts/MoveController.cs
+++ Assets/Scripts/MoveController.cs
@@ -18,11 +18,11 @@ public class MoveController : MonoBehaviour
 		{
 			Player.transform.Translate(Vector3.back * _speed * Time.deltaTime);
 		}
-<<<<<<< HEAD
+
 		if (Input.GetKeyDown(KeyCode.Space))
 		{
 			Player.transform.Translate(Vector3.up * _speed * Time.deltaTime);
-=======
+		}
 		if (Input.GetKey(KeyCode.A))
 		{
 			Player.transform.Translate(Vector3.left * _speed * Time.deltaTime);
@@ -30,7 +30,7 @@ public class MoveController : MonoBehaviour
 		if (Input.GetKey(KeyCode.D))
 		{
 			Player.transform.Translate(Vector3.right * _speed * Time.deltaTime);
->>>>>>> add-move-left
 		}
+
 	}
 }
